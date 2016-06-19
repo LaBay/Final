e6cb8d25b081f748ee48184832c46180c0115ec8
“LaBay” <labay@i.ua>
2016-06-19 15:42:42 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
 "use strict"
 
-var $left_btn = $('#step-section__content__item--1--left');
-var $right_btn = $('#step-section__content__item--1--right');
-var n = 0;
+
+ /* CAROUSEL */
+
+var n1 = 0;
+var n2 = 1;
+var n3 = 2;
+var k;
 var quantity = 3;
 
-function count(){
-	return Math.abs((quantity + n) % quantity)
+function count(n){
+	k = Math.abs((quantity + n) % quantity);
 };
 
-$left_btn.on('click', function(){
-	n++;
-	//console.log('n=', n, Math.abs((quantity + n) % quantity));
-	$(this).parent().css({background: 'url(background/step'+ count() + '_bg_d.jpg)'})
+function change(button){
+	button.parent().css({background: 'url(background/step'+ k + '_bg_d.jpg)'})
+};
+
+$('#step-section__content__item--1--left').on('click', function(){
+	n1--;
+	count(n1);
+	change($(this));
 });
 
+$('#step-section__content__item--1--right').on('click', function(){
+	n1++;
+	count(n1);
+	change($(this));
+});
 
-$right_btn.on('click', function(){
-	n--;
-//	console.log('n=', n, Math.abs((quantity + n) % quantity));
-	$(this).parent().css({background: 'url(background/step'+ count() + '_bg_d.jpg)'})
+$('#step-section__content__item--2--left').on('click', function(){
+	n2--;
+	count(n2);
+	change($(this));
+});
+
+$('#step-section__content__item--2--right').on('click', function(){
+	n2++;
+	count(n2);
+	change($(this));
+});
+
+$('#step-section__content__item--3--left').on('click', function(){
+	n3--;
+	count(n3);
+	change($(this));
+});
+
+$('#step-section__content__item--3--right').on('click', function(){
+	n3++;
+	count(n3);
+	change($(this));
 });
