3d6ac211fadc4471a3c89fb763061f2aae373da0
“LaBay” <labay@i.ua>
2016-06-21 02:29:58 +0300

    +

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var n3 = 2;
 var k;
 var quantity = 3;
 
+
 function count(n){
 	k = Math.abs((quantity + n) % quantity);
 };
@@ -55,7 +56,7 @@ $('#step-section__content__item--3--right').on('click', function(){
 
 
 	
-
+/* IDEA PHOTOS */
 
 $(function(){
 
@@ -64,18 +65,78 @@ const BEMclass = 'idea-section__content__item';
 var newElemets = '';
 var BEMselector = 1;
 var $queryBlock = $('#queryBlock');
+var fatItem1, fatItem2;
 var queries = ['Sports and Activity', 'Welness and Health', 'Extreme Sports and Expeditions', 'Games', 'Culture and Education', 'Relaxation', 'Travelling'];
+
+
 	function create(){
+		
 		$queryBlock.html('');
 
-		function fat(){
-			if ((BEMselector == 5) || (BEMselector == 6))	return ' fat';
+		function fatalizator(fat){
+		
+				if (fat < 0.08){
+					fatItem1 = 1;
+					fatItem2 = 3;
+				};
+				if (fat>0.08 && fat<0.17){
+					fatItem1 = 1;
+					fatItem2 = 4;
+				};
+				if (fat>0.17 && fat<0.25){
+					fatItem1 = 1;
+					fatItem2 = 6;
+				};		    
+				if (fat>0.25 && fat<0.33){
+					fatItem1 = 1;
+					fatItem2 = 7;
+				};
+
+				if (fat>0.33 && fat<0.42){
+					fatItem1 = 2;
+					fatItem2 = 3;
+				};
+				if (fat>0.42 && fat<0.5){
+					fatItem1 = 2;
+					fatItem2 = 4;
+				};
+				if (fat>0.5 && fat<0.58){
+					fatItem1 = 2;
+					fatItem2 = 6;
+				};			    
+				if (fat>0.58 && fat<0.67){
+					fatItem1 = 2;
+					fatItem2 = 7;
+				};
+
+				if (fat>0.67 && fat<0.75){
+					fatItem1 = 4;
+					fatItem2 = 6;
+				};
+				if (fat>0.75 && fat<0.83){
+					fatItem1 = 4;
+					fatItem2 = 7;
+				};
+				if (fat>0.83 && fat<0.92){
+					fatItem1 = 5;
+					fatItem2 = 6;
+				}		    
+				if (fat > 0.92){
+					fatItem1 = 5;
+					fatItem2 = 7;
+				};
+
+		}fatalizator(Math.random());
+
+
+		function fatInsert(){
+			if ((BEMselector == fatItem1) || (BEMselector == fatItem2))	return ' fat';
 			else return '';
 		};
 
 		while(BEMselector <= quantity){
 				let str = '<li class="' + BEMclass + ' ' + BEMclass + '--' 
-				+ BEMselector + fat() + '">' + '</li> ';
+				+ BEMselector + fatInsert() + '">' + '</li> ';
 				BEMselector++;
 				newElemets += str;
 		};
@@ -85,17 +146,40 @@ var queries = ['Sports and Activity', 'Welness and Health', 'Extreme Sports and 
 
 	}create();
 
-	function inner(){
+	var $block = $('.' + BEMclass);	
 
-		var $block = $('.' + BEMclass);
-		var $word = $('.' + BEMclass + ' span');
-		
-		for (let i = 0; i < $block.length; i++){
-			$.getJSON('http://api.pixplorer.co.uk/image?word=' + queries[i] + '&amount=1&?size=l(large)', function(r){
+	function getImage(el, i){
+
+		$.getJSON('http://api.pixplorer.co.uk/image?word=' + el + '&amount=1&?size=l(large)', function(r){
 				console.log(r)
 				$block.eq(i).css('background-image', 'url(' +r.images[0].imageurl+ ')')
 							.html('<span>' + r.images[0].word + '</span>')
-			})
+		});
+	};
+
+
+	function inner(){
+				//var $word = $('.' + BEMclass + ' span');
+		
+		for (let i = 0; i < $block.length; i++){
+			getImage((queries[i]), i);
 		}
+
 	}inner();
+
+
+	function search(arg){
+
+			for (let i = 0; i < $block.length; i++){
+				getImage(arg, i);
+			}
+	};
+
+
+	$('#interests-search_btn').on('click', function(){
+		let value = $('#interests-search_field').val();
+		search(value);
+	});
+
+
 })
